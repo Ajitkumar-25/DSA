e2238befae0a511cc6ca3a6106f8c9941644da27
Ajit kumar <kajit0408@gmail.com>
2022-11-26 22:25:40 +0530

    bst to min heap code added

--- HEAPS/kth largest.cpp
+++ HEAPS/kth largest.cpp
@@ -1,2 +1,15 @@
 #include<iostream>
 #include<queue>
+using namespace std;
+int main()
+{
+    priority_queue<int>pq;
+    pq.push(10);
+    pq.push(6);
+    pq.push(3);
+    pq.push(15);
+    pq.push(5);
+
+    
+
+}
